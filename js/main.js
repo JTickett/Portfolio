a85fa29f96d72678bb7e7a57c20a940cebafd8ac
JTickett <jtickett@gmail.com>
2024-06-18 17:25:06 +0100

    text effects complete and working on page load. jsconfig.json also added so that project detects Jquery and uses Intellisense.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,27 @@
 
 
-//  An effect to make the Banner Text gently fade in from transparent
-function fadeIn(){
-    $("#banner-section").fadeIn(3000);
-}
-//  Testing Fade Out
-function fadeOut(){
-    $("#banner-section").fadeOut(3000);
-}
-
-//  A subtle wave animation effect for the Banner Text
-function makeWave(){
-    const banner = document.querySelector("#banner-text");
-    banner.innerHTML = banner.textContent.replace(/\S/g, '<span class="wave">$&</span>');
-
-    const element = document.querySelectorAll("span");
-    for (let i=0; i<element.length; i++) {
-        element[i].style.animationDelay = i*0.10 + "s";
-    }
-}
-
-
+//  This event is on the Page Load, and it's main purpose is to trigger the text animations
 window.addEventListener('load', function () {
-    alert("It's loaded!");
+
+    //  Just for debug purposes :)
     console.log("Page Loaded");
 
-    
+    //  Start wave animation
+    makeWave()
+
+    //  Set it to Flex, Hide it again, *then* fade in.
+    //  This might seem weird, but it is kind of necessary to do it this way 
+    $("#banner-section").css("display","flex").hide().fadeIn(3000);
+
+    //  The idea was that this line would be spat out once the animation completed, but it doesn't appear to work that way at all... it's instant
+    console.log("Fading in done");
+
+})
 
 
-  })
+
+
+
 
 
 
@@ -89,4 +82,37 @@ function submitContactForm(){
     } else {
         //  Display validation error.
     }
+}
+
+
+
+
+
+
+
+//  A subtle wave animation effect for the Banner Text
+//  NOTE: Currently relying on Vanilla JS, but would like to convert to Jquery if possible :)
+function makeWave(){
+    const banner = document.querySelector("#banner-text");
+
+    banner.innerHTML = banner.textContent.replace(/\S/g, '<span class="wave">$&</span>');
+
+    const element = document.querySelectorAll("span");
+    for (let i=0; i<element.length; i++) {
+        element[i].style.animationDelay = i*0.10 + "s";
+    }
+}
+
+
+
+
+
+//  NOTE: These FADE functions are no longer used, but there's no harm in keeping them and they can help demonstrate the workings of it.
+//  An effect to make the Banner Text gently fade in from transparent
+function fadeIn(){
+    $("#banner-section").fadeIn(3000);
+}
+//  Testing Fade Out
+function fadeOut(){
+    $("#banner-section").fadeOut(3000);
 }
