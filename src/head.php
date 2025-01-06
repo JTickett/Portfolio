a2d535724023b0e7d063c88467dab4d0405b1db9
James Tickett <jtickett@gmail.com>
2025-01-06 18:51:39 +0000

    favicon added

--- src/head.php
+++ src/head.php
@@ -4,7 +4,13 @@
     
     <!-- Title & Favicon -->
     <title><?php echo isset($pageTitle) ? $pageTitle : 'James Tickett - Portfolio'; ?></title>
-    <!-- <link rel="icon" type="image/x-icon" href="images/favicon.ico"> -->
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/site.webmanifest">
+
 
     <!-- Stylesheets -->
     <!-- <link rel="stylesheet" type="text/css" href="css/normalize.css"> -->
